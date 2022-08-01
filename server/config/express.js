d70edfd8267dc54bef3bcac71f069b82328f5603
talal <talalbadreddine@hotmail.com>
2022-08-01 12:25:43 +0300

    serverConfig

--- server/config/express.js
+++ server/config/express.js
@@ -1,17 +1,39 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 
-dotenv.config({path: __dirname + '/../../.env'})
+dotenv.config({path: __dirname + '/../.env'})
 
 const {
     dbPort,
     dbHost,
-    dbName
+    dbName,
+    serverPort
 } = process.env
 
 async function connectDB(){
-    const uri = ``
+    const uri = `mongodb://${dbHost}:${dbPort}/${dbName}`
     await mongoose.connect(uri)
     console.log("Connected to db!")
 }
+
+async function startServer(){
+    try{
+        await connectDB()
+
+        // intialize express app
+        const app = express()
+
+
+        // Insert Routest here
+
+        app.listen(serverPort, () => console.log(`Listening to port ${serverPort}`))
+
+    }catch(error){
+       
+        console.log(`Error server connection with Db : ${error.message}`)
+    }
+}
+
+startServer()
