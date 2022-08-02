6bc211d28dd17b52c69a14314a4c16b1c847b9b0
talal <talalbadreddine@hotmail.com>
2022-08-02 11:05:01 +0300

    auth middleware added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,18 +1,21 @@
 const router = require('express').Router
 const userRouter = router()
 const {
+    validateUser
+  } = require('../middleware/auth')
+
+const {
     getWareHousesForUsers,
     userLogin,
     userRegister
 } = require('../controllers/userController')
 
 
-
-userRouter.get('/getWarehouses', getWareHousesForUsers )
+userRouter.get('/getWarehouses', validateUser, getWareHousesForUsers )
 
 userRouter.post('/login', userLogin)
 
-userRouter.post('/registerUser', userRegister)
+userRouter.post('/register', userRegister)
 
 module.exports = {
     userRouter,
