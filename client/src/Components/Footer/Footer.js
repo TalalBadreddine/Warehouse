cd50f8915bc049d8803f4bd4d43f988669552225
KhalilAbbaass <103119609+KhalilAbbaass@users.noreply.github.com>
2022-08-06 18:23:08 +0300

    to fix

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -9,7 +9,7 @@ export const Footer = () => {
     <Outlet></Outlet>
     <div>
 
-    <CDBFooter className="shadow" style={{color: "white", backgroundColor:"#767B78", position:"absolute" ,bottom:0, left:0, right:0}} >
+    <CDBFooter className="shadow" style={{color: "white", backgroundColor:"#767B78", position:"static" ,bottom:'0', left:'0', right:'0',display:'flex'}} >
       <CDBBox display="flex" flex="column" className="mx-auto py-2" style={{ width: '90%' }}>
 
         <CDBBox display="flex" justifyContent="between" className="flex-wrap">
