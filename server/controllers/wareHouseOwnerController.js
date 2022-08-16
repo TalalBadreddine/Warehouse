4e9c0921734b5070d811f0b8fccd9b7d2450bb12
talal <talalbadreddine@hotmail.com>
2022-08-16 10:03:23 +0300

    adding payment

--- server/controllers/wareHouseOwnerController.js
+++ server/controllers/wareHouseOwnerController.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const warehouseOwnerModel = require('../models/WarehouseOwner');
 const jwt = require('jsonwebtoken');
-const warehouseSchema=require('../models/warehouseSchema');
+const warehouseSchema = require('../models/warehouseSchema');
 const crypto = require('crypto');
 const dotenv = require('dotenv')
 const jwtDecode = require('jwt-decode');
@@ -9,73 +9,104 @@ const manageUsersAndWarehousesSchema = require('../models/manageUsersAndWarehous
 const extensions = require('../helper/extensions');
 
 
-
-dotenv.config({path: __dirname + '/../.env'})
+dotenv.config({ path: __dirname + '/../.env' })
 
 const {
     hashType,
     encodeAs,
-    jwtSecret
+    jwtSecret,
+    stripeSecretKey
 } = process.env
 
+const stripe = require('stripe')(`${stripeSecretKey}`);
 
 //Register
 const register = async (req, res) => {
     try {
 
-        const {userName, email, password,phoneNumber,cardNumber,cardExpires,cardCode,cardName} = req.body;
-        
-            
-            await warehouseOwnerModel.create({
-                userName: userName,
-                email: email,
-                password: crypto.createHash(hashType).update(password).digest(encodeAs),
-                phoneNumber: phoneNumber,
-                cardNumber: cardNumber,
-                cardExpires:cardExpires,
-                cardCode:cardCode,
-                cardName:cardName         })
+        const { userName, email, password, phoneNumber, cardNumber, cardExpires, cardCode, cardName } = req.body;
 
-            return res.send('created').status(200)
+        const alreadyExist = warehouseOwnerModel.find({
+            email: email
+        })
 
-    } catch(error){
+        if(alreadyExist.length > 0){
+            return res.send('alreadyExist').status(403)
+        }
+
+        const account = await stripe.accounts.create({
+            country: 'US',
+            type: 'express',
+            capabilities: {
+                card_payments: { requested: true },
+                transfers: { requested: true },
+            },
+        });
+
+        console.log(account)
+
+        await warehouseOwnerModel.create({
+            userName: userName,
+            email: email,
+            password: crypto.createHash(hashType).update(password).digest(encodeAs),
+            phoneNumber: phoneNumber,
+            cardNumber: cardNumber,
+            cardExpires: cardExpires,
+            cardCode: cardCode,
+            cardName: cardName,
+            stripeAccountId: account.id
+        })
+
+        const accountLink = await stripe.accountLinks.create({
+            account: `${account.id}`,
+            refresh_url: `http://localhost:3000/login`,
+            return_url: 'http://localhost:3000/login',
+            type: 'account_onboarding',
+            collect: 'currently_due',
+          });
+
+          console.log(accountLink)
+
+        return res.send(accountLink).status(200)
+
+    } catch (error) {
         console.log(error)
-        res.status(500).json({message : "an error occured at register function "});
+        res.status(500).json({ message: "an error occured at register function " });
     }
 }
 
 //Login
 const login = async (req, res) => {
     try {
-    
-        const {email, password} = req.body; 
+
+        const { email, password } = req.body;
 
         const user = await warehouseOwnerModel.findOne({
             email
         })
 
-        if(!user) return res.status(400).json({message : "user does not exist"});        
+        if (!user) return res.status(400).json({ message: "user does not exist" });
 
         // verify password
-        if(( crypto.createHash(hashType).update(password).digest(encodeAs) != user.password)) 
-            return res.status(403).json({message : "incorrect password!"})
+        if ((crypto.createHash(hashType).update(password).digest(encodeAs) != user.password))
+            return res.status(403).json({ message: "incorrect password!" })
 
         // create token
         const payload = {
-            user          
+            user
         }
-        
-        await jwt.sign({user: user, role: 'warehouseOwner'}, jwtSecret, async (err, token) => {
+
+        await jwt.sign({ user: user, role: 'warehouseOwner' }, jwtSecret, async (err, token) => {
 
             await res.cookie('jwt', `${token}`, { httpOnly: true })
-             res.status(200).json(token)
+            res.status(200).json(token)
         })
 
 
 
-    } catch(error){
+    } catch (error) {
         console.log(error)
-         res.status(500).json({message : "an error occured at login function"});
+        res.status(500).json({ message: "an error occured at login function" });
     }
 }
 
@@ -83,54 +114,54 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
     try {
 
-        if(req.cookies.token){
+        if (req.cookies.token) {
             res.clearCookie("token");
             return res.sendStatus(200);
-        }else{
+        } else {
             return res.sendStatus(400);
         }
 
-    } catch(error){
+    } catch (error) {
         console.log(error)
-        return res.status(500).json({message : "an error occured at logout function"});
+        return res.status(500).json({ message: "an error occured at logout function" });
     }
 }
 
 const getRequests = async (req, res) => {
-    try{
+    try {
         const userInfo = jwtDecode(req.cookies['jwt'])
         const results = await manageUsersAndWarehousesSchema.find({
-            status:'pending',
+            status: 'pending',
             warehouseOwnerEmail: userInfo.user.email
         })
 
         return res.send(results).status(200)
     }
-    catch(err){
+    catch (err) {
         console.log(`error at getRequest function ${err.message}`)
     }
 }
 
 const acceptDeclineRequest = async (req, res) => {
-    try{
+    try {
         //TODO: talal check what is the use of decoded Info
         const decodedInfo = jwtDecode(req.cookies['jwt'])
         const requestId = req.body.requestId
-        let requestStatus =  req.body.status
+        let requestStatus = req.body.status
         const warehouseId = req.body.warehouseId
         const requestedDate = req.body.requestedDate
-   
 
-        if(requestStatus == 'accepted'){
-            
-            await extensions.userRentAWarehouseInSpecificDate(warehouseId,requestedDate ).then( async (response) => {
+
+        if (requestStatus == 'accepted') {
+
+            await extensions.userRentAWarehouseInSpecificDate(warehouseId, requestedDate).then(async (response) => {
                 console.log(response)
-                if(response){ requestStatus = 'accepted'}else{requestStatus = 'rejected'}
+                if (response) { requestStatus = 'accepted' } else { requestStatus = 'rejected' }
 
                 await manageUsersAndWarehousesSchema.updateOne({
                     _id: requestId
-                },{
-                    $set:{
+                }, {
+                    $set: {
                         status: requestStatus
                     }
                 }
@@ -138,9 +169,9 @@ const acceptDeclineRequest = async (req, res) => {
             })
         }
         return res.send('updated').status(200)
-        
+
     }
-    catch(err){
+    catch (err) {
         console.log(`error at acceptDeclineRequest => ${err.message}`)
     }
 }
@@ -149,26 +180,26 @@ const acceptDeclineRequest = async (req, res) => {
 
 const addWarehouses = async (req, res) => {
 
-    try{
-        const  warehouse = req.body;
+    try {
+        const warehouse = req.body;
         const result = await warehouseSchema.create(warehouse);
-        if(result){
-            res.status(201).json({message:"added WareHouse"})
-            
-           
-        }else{
-            res.status(409).json({message:"failed to add WareHouse"})
+        if (result) {
+            res.status(201).json({ message: "added WareHouse" })
+
+
+        } else {
+            res.status(409).json({ message: "failed to add WareHouse" })
         }
-    
-    }catch(error){
-        res.status(500).json({message:"error at addWarehouse function"})
-    
+
+    } catch (error) {
+        res.status(500).json({ message: "error at addWarehouse function" })
+
     }
 }
 
 // GET request for a list of all warehouseOwner
 const getWarehouses = async (req, res) => {
-    try{
+    try {
 
         const decodedInfo = jwtDecode(req.cookies['jwt'])
         let myWarehousese = []
@@ -177,38 +208,38 @@ const getWarehouses = async (req, res) => {
             email: decodedInfo.user.email
         })
 
-      for(let i = 0 ; i < owner.myWarehouses.length ; i++){
+        for (let i = 0; i < owner.myWarehouses.length; i++) {
 
-        let warehouse = await warehouseSchema.find({
-            _id: owner.myWarehouses[i]
-        })
+            let warehouse = await warehouseSchema.find({
+                _id: owner.myWarehouses[i]
+            })
 
-        myWarehousese.push(warehouse[0])
-      }
-
-      return res.status(200).send(myWarehousese)
-}
- catch(error){
-     res.status(500).json({message : "internal error with function get all warehouses"})
-
- }
-}
-
-
-    // DELETE request to delete a WarehouseOwner
-    const  deleteWarehouse = async(req, res) => {
-      
-          try{
-              const _Id= req.body._Id
-              await warehouseSchema.findOneAndDelete({_Id:_Id})
-              res.status(200).json({message : "warehouse deleted"})
+            myWarehousese.push(warehouse[0])
         }
-        catch(error){
-            res.status(500).json({message :"internal error with fucntion delete warehouse"})
-    
-        }
-      
+
+        return res.status(200).send(myWarehousese)
     }
+    catch (error) {
+        res.status(500).json({ message: "internal error with function get all warehouses" })
+
+    }
+}
+
+
+// DELETE request to delete a WarehouseOwner
+const deleteWarehouse = async (req, res) => {
+
+    try {
+        const _Id = req.body._Id
+        await warehouseSchema.findOneAndDelete({ _Id: _Id })
+        res.status(200).json({ message: "warehouse deleted" })
+    }
+    catch (error) {
+        res.status(500).json({ message: "internal error with fucntion delete warehouse" })
+
+    }
+
+}
 
 
 
